920ea82dde43fcbf6c5bc963cb9a6856915537af
Tarunkumar0089 <tarunkumar321028@gmail.com>
2025-10-04 23:18:35 +0530

    assignment

---
+++ day_07/Maximum_Subarray.java
@@ -0,0 +1,14 @@
+package day_07;
+import java.util.*;
+public class Maximum_Subarray {
+    public static void main(String[] args) {
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+        int maxSoFar=nums[0];
+        int maxEndingHere=nums[0];
+        for(int i=1;i<nums.length;i++){
+            maxEndingHere=Math.max(nums[i],maxEndingHere+nums[i]);
+            maxSoFar=Math.max(maxSoFar,maxEndingHere);
+        }
+        System.out.println(maxSoFar);
+    }
+}
